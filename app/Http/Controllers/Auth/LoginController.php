f085ba2a2be67089fda9a50991bd596b02809214
sempya <igoryainov@gmail.com>
2018-01-30 13:12:21 +0300

     

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,12 +20,20 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    //
+    protected $loginView;
+
+    protected $username = 'login';
+
+    //
+    //protected $username = 'login';
+
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
@@ -35,5 +43,23 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        //
+        $this->loginView = env('THEME') . '.login';
+        //
+    }
+
+    public function showLoginForm()
+    {
+        $view = property_exists($this, 'loginView')
+            ? $this->loginView : '';
+
+        if (view()->exists($view))
+        {
+            return view($view)->with('title', 'Вход на сайт');
+        }
+
+        abort(404);
+
+        //return view(env('THEME') . '.login')->with('title', 'Вход на сайт');
     }
 }
